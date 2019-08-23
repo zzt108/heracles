ee32db7bf08da10758ba203951ecb086771f5309
Zsolt Toth <gerzson63@gmail.com>
2019-08-23 11:21:20 +0200

    Added BI to Controller

---
+++ Service/Controller/Format.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller
+{
+    public class Format
+    {
+        public string Money(string input, int rounding = 2)
+        {
+            try
+            {
+                if (decimal.TryParse(input, out var number))
+                {
+                    var rounded = Math.Round(number, rounding);
+                    return string.Format("# ##0.00", number);
+                }
+                else
+                {
+                    throw new ArgumentException($"Cannot parse '{input}'", nameof(input));
+                }
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException(e.Message, nameof(input));
+            }
+        }
+    }
+}
