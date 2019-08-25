6e3a8c6c000c5a0d00665b6900d178eb3bdbe626
Zsolt Toth <gerzson63@gmail.com>
2019-08-25 18:04:47 +0200

    No backend UI test added

--- Service/Tests/IntegrationTests/UITests.cs
+++ Service/Tests/IntegrationTests/UITests.cs
@@ -5,10 +5,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-using Selenium.Interfaces;
-using Selenium;
 using System.Configuration;
+using OpenQA.Selenium;
+using FluentAssertions;
 
 namespace IntegrationTest
 {
@@ -19,9 +18,9 @@ namespace IntegrationTest
         private static string WebSiteRoot { get; set; }
 
         static UITests()
-		{
-			WebSiteRoot = ConfigurationManager.AppSettings["WebSiteRoot"];
-		}
+        {
+            WebSiteRoot = ConfigurationManager.AppSettings["WebSiteRoot"];
+        }
 
         public UITests(Selenium.Selenium.BrowserTypeEnum browserType)
         {
@@ -29,12 +28,23 @@ namespace IntegrationTest
         }
 
         [Test]
-        public void CanOpenUI()
+        public void CanOpenUIWithoutBackend()
         {
             using (var selenium = new Selenium.Selenium(WebSiteRoot + "client.html", _browserType))
             {
-                selenium.Driver.Navigate().GoToUrl(selenium.BaseUrl);
+                var driver = selenium.Driver;
+                driver.Navigate().GoToUrl(selenium.BaseUrl);
+                var inputNumber = driver.FindElement(By.Id("aidNumber"));
+                var sendButton = driver.FindElement(By.Id("aidCallService"));
+                inputNumber.Clear();
+                inputNumber.SendKeys("1234");
+                sendButton.Click();
+
+                var errorMessage = driver.FindElement(By.Id("aidError"));
+                errorMessage.Should().NotBeNull("There should be an error message about missing service");
+                errorMessage.Text.Should().Be("No API Error", "No API Error message expected");
             }
         }
+
     }
 }
